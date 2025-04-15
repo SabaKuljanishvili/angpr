adcc70cd65461d669ba793f331984d9c18e5b688
saba kuljanishvili <kuljanisaba@gmail.com>
2025-04-15 21:54:19 +0400

    rout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CancelComponent } from './cancel/cancel.component';
+import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: "home", pathMatch: "full"  }, 
+    {path: 'home', component: HomeComponent},
+    {path: 'cancel', component: CancelComponent},
+    {path: 'about', component: AboutComponent},
+    {path: '**', component: ErrorComponent },
+
+
+
+    {path: "", redirectTo: "home", pathMatch: "full"},
+   
+    {
+       path: "home",
+       title: "Home",
+       loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+    },
+    {
+      path: "cancel",
+      title: "Tickets cancellation",
+      loadComponent: () => import('./cancel/cancel.component').then(m => m.CancelComponent)
+   },
+    {
+        path: "about",
+        title: "About",
+        loadComponent: () => import('./about/about.component').then(m => m.AboutComponent)
+     },
+];
