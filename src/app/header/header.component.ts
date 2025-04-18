e220fad81f8308bf7c6bf644029bd0e35c0a3cc5
sabakuljanishvili <kuljanisaba@gmail.com>
2025-04-18 12:56:10 +0400

    header resp + burger button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,10 +3,22 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
      activeRouteClass = "active"
+
+
+     isMenuOpen = false;
+  
+     toggleMenu(): void {
+       this.isMenuOpen = !this.isMenuOpen;
+     }
+     
+     closeMenu(): void {
+       this.isMenuOpen = false;
+     }
 }
